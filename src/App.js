97ab26f096e433d434e774c8de5c785bb53673bd
Erika Putrim <eputrim@gmail.com>
2021-03-26 20:20:46 +0700

    workspace preparation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+
+import 'assets/scss/style.scss'
 
 function App() {
   return (
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import moduleName from 'elements/Button'
+
+export default function LandingPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
