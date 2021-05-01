5646b6fde33b2d1fd1ada3cd0517438f531c7778
Erika Putrim <eputrim@gmail.com>
2021-05-01 22:42:21 +0700

    Section Testimonial

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,11 +1,53 @@
 import React from "react";
 
-import Star from "elements/Star";
+import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
 
-export default function Testimony() {
+import Star from "elements/Star";
+import Button from "elements/Button";
+
+export default function Testimony({ data }) {
   return (
-    <div>
-      <Star value={3.4} width={35} height={35} spacing={4}></Star>
-    </div>
+    <section className="container">
+      <div class="row align-items-center">
+        <div class="col-auto" style={{ marginRight: 60 }}>
+          <div class="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+            <img
+              src={data.imageUrl}
+              alt="Testimonial"
+              className="position-absolute"
+              style={{ zIndex: 1 }}
+            />
+            <img
+              src={TestimonyAccent}
+              alt="Testimonial frame"
+              className="position-absolute"
+              style={{ margin: `-30px 0 0 -30px` }}
+            />
+          </div>
+        </div>
+        <div className="col">
+          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+          <Star value={data.rate} width={35} height={35} spacing={4} />
+          <h5 class="h2 font-weight-light line-height-2 my-3">
+            {data.content}
+          </h5>
+          <span class="text-gray-500">
+            {data.familyName}, {data.familyOccupation}
+          </span>
+          <div>
+            <Button
+              type="link"
+              href={`/testimonial/${data._id}`}
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              hasShadow
+              isPrimary
+            >
+              Read Their Story
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
