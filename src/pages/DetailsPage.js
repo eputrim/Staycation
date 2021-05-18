9eee1e83d401ec8a3062987c3c58c7762e0ef303
Erika Putrim <eputrim@gmail.com>
2021-05-18 14:37:47 +0700

    Page Details: treasures, testimonial, footer

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 import Header from "parts/Header";
 import PageDetails from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import ItemDetails from "json/itemDetails.json";
 
@@ -22,19 +26,26 @@ export default class DetailsPage extends Component {
 
     return (
       <>
-        <Header {...this.props}></Header>
-        <PageDetails breadcrumb={breadcrumb} data={ItemDetails}></PageDetails>
-        <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <Header {...this.props} />
+        <PageDetails breadcrumb={breadcrumb} data={ItemDetails} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
-              <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
             </div>
             <div className="col-5">
-              <BookingForm itemDetails={ItemDetails} />
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
             </div>
           </div>
         </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
