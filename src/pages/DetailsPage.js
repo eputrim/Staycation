8a91a4fdcea32b4262a982ab9936f10220db38d4
Erika Putrim <eputrim@gmail.com>
2021-05-18 13:42:26 +0700

    Page Details: description and feature

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import PageDetails from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
+import PageDetailDescription from "parts/PageDetailDescription";
 import ItemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
@@ -22,6 +23,14 @@ export default class DetailsPage extends Component {
         <Header {...this.props}></Header>
         <PageDetails breadcrumb={breadcrumb} data={ItemDetails}></PageDetails>
         <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+            </div>
+            <div className="col-5">Booking Form</div>
+          </div>
+        </section>
       </>
     );
   }
